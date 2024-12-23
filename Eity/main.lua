528d5dcfb4502fd8c65dc5118aa7a7fc3ed4c4f4
Virus <103518350+Virus01Official@users.noreply.github.com>
2024-12-23 12:41:26 +0100

    Added window resizing
    Why wasn't it in the game?!

--- Eity/main.lua
+++ Eity/main.lua
@@ -14,11 +14,13 @@ function love.load()
   gameManager:load()
   stateManager:load()
   love.graphics.setBackgroundColor(0.1, 0.1, 0.1, 1)
-  simpleScale.setWindow(gw, gh, resolutionList[saveManager.settings.resolutionIndex][1], resolutionList[saveManager.settings.resolutionIndex][2])
+   -- Set the window to be resizable
+  love.window.setMode(resolutionList[saveManager.settings.resolutionIndex][1], resolutionList[saveManager.settings.resolutionIndex][2], { resizable = true })
+  simpleScale.setWindow(gw, gh, resolutionList[saveManager.settings.resolutionIndex][1], resolutionList[saveManager.settings.resolutionIndex][2], {resizable = true})
   discordRPC.initialize(appId, true)
   now = os.time(os.date("*t"))
   detailsNow = "In Mainmenu"
-  stateNow = ""
+  stateNow = ""  
   nextPresenceUpdate = 0
 end
 
@@ -30,7 +32,7 @@ function discordApplyPresence()
     detailsNow = "In Mainmenu"
     stateNow = ""
   end
-
+  
   presence = {
     largeImageKey = "eity_icon",
     largeImageText = "Eity v1.0.0",
@@ -38,7 +40,7 @@ function discordApplyPresence()
     state = stateNow,
     startTimestamp = now,
   }
-
+  
   return presence
 end
 
@@ -47,30 +49,31 @@ function love.gamepadpressed(joystick, button)
 end
 
 function love.update(dt)
-  collectgarbage()
+  collectgarbage() 
   mx = love.mouse.getX() / simpleScale.getScale()
   my = love.mouse.getY() / simpleScale.getScale()
   scoreManager:update(dt)
   gameManager:update(dt)
   stateManager:update(dt)
-
+  
   if saveManager.settings.isEnabledVSync then
     love.window.setVSync(1)
   else
     love.window.setVSync(0)
   end
-
+  
   if nextPresenceUpdate < love.timer.getTime() then
       discordRPC.updatePresence(discordApplyPresence())
       nextPresenceUpdate = love.timer.getTime() + 2.0
   end
   discordRPC.runCallbacks()
-
-
+  
+  -- Update the window size and scaling when resizing
+  simpleScale.resizeUpdate()
 end
 
 
-function love.draw()
+function love.draw()  
 	simpleScale.set()
     stateManager:draw()
     if saveManager.settings.isEnabledFPS then
